48ce06e05cdff050f416db56ddecdda26df09be5
Victor Campos <victorboaventcampos@gmail.com>
2021-09-10 23:28:51 -0300

    Modal Nova Consulta

---
+++ components/consulta/modalCreate.js
@@ -0,0 +1,25 @@
+export default {
+    name: 'modalCreate',
+    props: ['value'],
+    data() {
+        return {
+            date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+            dataConsulta: '',
+            paciente:'',
+            medico:'',
+            menuDataConsulta:false
+
+        }
+    },
+    mounted() {
+        //this.listaPacientes();
+    },
+    methods: {
+        formatDate(date) {
+            if (!date) return null
+            const [year, month, day] = date.split('-')
+            return `${day}/${month}/${year}`
+        }
+
+    }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    Aqui vai ser a agenda
-  </div>
+    <v-container>
+    <v-row>
+      <v-col :md="10" :offset-md="1" class="consulta-container">
+        <v-btn color="primary" large @click="modalAtivo = !modalAtivo">
+          Marcar Consulta
+        </v-btn>
+        <modalCreateConsulta v-model="modalAtivo" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import modalCreateConsulta from '@/components/consulta/modalCreate.vue'
 export default {
-  layout: 'main'
+  layout: 'main',
+  components: {
+    modalCreateConsulta
+  },
+  data(){
+    return {
+      modalAtivo: false
+    }
+  }
 }
 </script>
 
 <style>
-
+  .consulta-container{
+    margin-top: 20px;
+    border-radius: 10px;
+    background: #fff;
+  }
 </style>
