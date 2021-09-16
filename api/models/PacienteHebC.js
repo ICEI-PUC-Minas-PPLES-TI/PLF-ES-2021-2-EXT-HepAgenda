e43dd281160164014042f2e51f14dbdd5ffba460
guizombas <guilherme.gabriel.532@gmail.com>
2021-09-16 18:27:31 -0300

    add models de pacientehep b e c

---
+++ api/models/PacienteHebC.js
@@ -0,0 +1,80 @@
+const { Model, DataTypes } = require('sequelize');
+const Paciente = require('./Paciente');
+const Tratamento = require('./Tratamento');
+
+class PacienteHepC extends Model{
+    static init (sequelize){
+        super.init({
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+            },
+            paciente_id:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: Paciente,
+                    key: 'id'
+                }
+            },
+            cirrotico: {
+                type: DataTypes.TINYINT(1),
+                allowNull: false
+            },
+            portador_inativo: {
+                type: DataTypes.TINYINT(1),
+                allowNull: false
+            },
+            fibrose: {
+                type: DataTypes.ENUM(['F0', 'F1', 'F2', 'F3', 'F4', 'F5']),
+                allowNull: false
+            },
+            tratado: {
+                type: DataTypes.TINYINT(1)
+            },
+            tratamento_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: Tratamento,
+                    key: 'id'
+                }
+            },
+            num_tratamentos: {
+                type: DataTypes.TINYINT.UNSIGNED,
+            },
+            inicio_tratamento: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_alfa: {
+                type: DataTypes.DOUBLE,
+            },
+            data_alfa: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_alfa: {
+                type: DataTypes.DOUBLE,
+            },
+            data_alfa: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_ultra: {
+                type: DataTypes.DOUBLE,
+            },
+            data_ultra: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_carga: {
+                type: DataTypes.DOUBLE,
+            },
+            data_carga: {
+                type: DataTypes.DATE,
+            },
+        }, {
+            tableName: "paciente_hepc",
+            timestamps: false,
+            sequelize
+        })
+    }
+}
+module.exports = PacienteHepC
---
+++ api/models/PacienteHepB.js
@@ -0,0 +1,71 @@
+const { Model, DataTypes } = require('sequelize');
+const Paciente = require('./Paciente');
+const Tratamento = require('./Tratamento');
+
+class PacienteHepB extends Model{
+    static init (sequelize){
+        super.init({
+            paciente_id:{
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                references: {
+                    model: Paciente,
+                    key: 'id'
+                }
+            },
+            cirrotico: {
+                type: DataTypes.TINYINT(1),
+                allowNull: false
+            },
+            portador_inativo: {
+                type: DataTypes.TINYINT(1),
+                allowNull: false
+            },
+            fibrose: {
+                type: DataTypes.ENUM(['F0', 'F1', 'F2', 'F3', 'F4', 'F5']),
+                allowNull: false
+            },
+            tratamento_id: {
+                type: DataTypes.INTEGER,
+                references: {
+                    model: Tratamento,
+                    key: 'id'
+                }
+            },
+            inicio_tratamento: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_alfa: {
+                type: DataTypes.DOUBLE,
+            },
+            data_alfa: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_alfa: {
+                type: DataTypes.DOUBLE,
+            },
+            data_alfa: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_ultra: {
+                type: DataTypes.DOUBLE,
+            },
+            data_ultra: {
+                type: DataTypes.DATE,
+            },
+            ultimo_resultado_carga: {
+                type: DataTypes.DOUBLE,
+            },
+            data_carga: {
+                type: DataTypes.DATE,
+            },
+        }, {
+            tableName: "paciente_hepb",
+            timestamps: false,
+            sequelize
+        })
+    }
+}
+
+module.exports = PacienteHepB
