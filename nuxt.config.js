97ca26f24c20ba0534c6f443202297af0f475965
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-11-29 22:58:53 -0300

    Atualizar basURL do axios no nuxtconfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
 
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: `http://localhost:${process.env.PORT}/api`,
+    baseURL: `/api`,
     timeout: 5000
   },
 
